98d645a304e39e936475f39e200e359b9e4530be
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2024-04-15 19:13:53 +0800

    init interaction.py

--- interaction.py
+++ interaction.py
@@ -1,13 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-
-total_articles = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
-print(total_articles.text)
-
-driver.quit()
+driver.get("http://secure-retreat-92358.herokuapp.com/")
