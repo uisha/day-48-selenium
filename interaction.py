efdf4df7ca7d09dc24c96efafabf12c9e5438ce6
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2024-04-15 19:29:19 +0800

    finished challenge

--- interaction.py
+++ interaction.py
@@ -7,3 +7,15 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://secure-retreat-92358.herokuapp.com/")
+
+first_name = driver.find_element(By.NAME, value="fName")
+first_name.send_keys("Joshua", Keys.ENTER)
+
+last_name = driver.find_element(By.NAME, value="lName")
+last_name.send_keys("Tipon", Keys.ENTER)
+
+email = driver.find_element(By.NAME, value="email")
+email.send_keys("joshtipon921@gmail.com", Keys.ENTER)
+
+
+# driver.quit()
